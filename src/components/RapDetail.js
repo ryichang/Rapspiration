36d0bf0f4629ba99d050953ccc1e5c7114747447
ryichang <ryichang@gmail.com>
2016-10-19 16:47:49 -0700

    Added react-addons-css-transition-group & twitter link

--- src/components/RapDetail.js
+++ src/components/RapDetail.js
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux';
 import RapImage from './RapImage';
 import RapArtist from './RapArtist';
 import RapLyric from './RapLyric';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import { fetchNewRap } from '../actions/index';
 
 
@@ -22,16 +23,22 @@ class RapDetail extends Component {
 	render() {
     // const currentRap = this.props.raps.currentRap;
     const { currentRap } = this.props.raps;
-
     console.log('raps', this.props.raps.currentRap)
     console.log('rap', currentRap)
+
+
 		return (
-      <div className="col-md-12 rapCard">
+      <div className="col-md-12 rapContainer">
         <div onClick={this.onClick}>
         <RapImage image={currentRap.image} />
         </div>
-        <RapArtist artist={currentRap.artist}/>
-        <RapLyric lyric={currentRap.lyric}/>
+        <RapArtist artist={currentRap.artist} key={currentRap.artist}/>
+        <ReactCSSTransitionGroup
+            transitionName="example"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={300}>
+        <RapLyric lyric={currentRap.lyric} key={currentRap.id}/>
+        </ReactCSSTransitionGroup>
       </div>
     );
 	}
--- src/components/RapLyric.js
+++ src/components/RapLyric.js
@@ -5,13 +5,22 @@ class RapLyric extends Component {
 	render() {
     const lyric = this.props.lyric
 		console.log('lyric is ', lyric)
+    const lyricContent = lyric.split(' ');
+    const lyricString = "";
+    const tweet = 'https://twitter.com/intent/tweet?text='+lyric+'%20%40RapInspiration&url=http%3A%2F%2Fwww.rapinspiration.com%2F&hashtags=rapinspiration&related=rxneu'
+    console.log('tweet is', tweet)
+
+
 		return (
       <div className="col-md-12 rapLyric">
         <h1>{lyric}</h1>
+        <a href={tweet} target="_blank">
+        <i className="fa fa-twitter twitter" aria-hidden="true"></i>
+        </a>
       </div>
     );
 	}
 }
-
+// a href="https://twitter.com/intent/tweet?text=hello"
 
 export default RapLyric;
