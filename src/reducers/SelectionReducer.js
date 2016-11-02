07068a9ace721243623909de7c544583ea6c2bbe
ryichang <ryichang@gmail.com>
2016-11-02 14:19:32 -0700

    Added favicon, added album, artist, lyric json files

--- src/reducers/SelectionReducer.js
+++
@@ -1,8 +0,0 @@
-// export default function (state = null, action) {
-//   switch(action.type) {
-//     case 'select_rap':
-//       return action.payload;
-//     default:
-//       return state;
-//   }
-// }
