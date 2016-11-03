53c0e1a458783a29977790a3278a2664c659be4f
ryichang <ryichang@gmail.com>
2016-11-03 16:21:36 -0700

    Included AlbumReducer in reducer index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import LyricReducer from './LyricReducer';
 import ArtistReducer from './ArtistReducer';
+import AlbumReducer from './AlbumReducer';
 import RapDetailReducer from './RapDetailReducer';
 
 const rootReducer = combineReducers({
   rapDetail: RapDetailReducer,
   lyrics: LyricReducer,
-  artist: ArtistReducer
+  artist: ArtistReducer,
+  albums: AlbumReducer
 });
 
 export default rootReducer;
