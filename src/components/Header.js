18ffec3eb610aa2ec6aebd3aa2542390d8382033
ryichang <ryichang@gmail.com>
2016-12-02 14:34:18 -0800

    Changed to container-fluid 90% width

--- src/components/Header.js
+++ src/components/Header.js
@@ -42,10 +42,10 @@ class Header extends Component {
             </Modal>
           </div>
           {/* <h3 className="logoName">Rapspiration</h3> */}
-          <a href="/" target="_blank">
+          <a href="/">
             <img src={"../img/LogoGrey.png"} className="logo"></img>
           </a>
-          <div className="sidebarCenter" onClick={this.onOpen}>
+          {/* <div className="sidebarCenter" onClick={this.onOpen}>
             <i className="fa fa-plug twitter" aria-hidden="true"></i>
           </div>
             <div className="sidebarBottom">
@@ -53,9 +53,15 @@ class Header extends Component {
               <i className="fa fa-twitter twitter" aria-hidden="true"></i>
               </a>
               <i className="fa fa-instagram" aria-hidden="true"></i>
-            </div>
+            </div> */}
         </div>
-        {/* <div id="top-spacer"></div> */}
+        <nav className="nav-contain">
+          <a href="/"><div className="home-btn nav-icon btn btn-round">Home</div></a>
+          <a href="https://twitter.com/RxNeu" target="_blank"><div className="fa fa-twitter btn btn-round"></div></a>
+          <a href=""  target="_blank"><div className="fa fa-instagram btn btn-round"></div></a>
+          <a href="#" ><div className="fa fa-eye btn btn-round" onClick={this.onOpen}></div></a>
+        </nav>
+
       </div>
     );
   }
