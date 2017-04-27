cf6f31b007da41e9a94768a98bc6e4e88834c805
ryichang <ryichang@gmail.com>
2017-04-27 15:29:25 -0700

    Changed server.js and package.json

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'))
 });
 
 app.listen(port);
-console.log('Server started')
+console.log('Server Started');
