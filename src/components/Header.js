5d554feda325888aa86a940c4fd1e43dca2a9821
ryichang <ryichang@gmail.com>
2018-08-12 22:36:13 -0700

    Changed tweets and updated modal details

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import { Nav, Navbar, NavItem, NavDropdown, MenuItem, Tooltip, OverlayTrigger} from 'react-bootstrap';
-import { Modal, Button } from 'react-bootstrap';
-
+import { Modal, Button } from "react-bootstrap";
 
 class Header extends Component {
-  constructor(props)  {
+  constructor(props) {
     super(props);
     this.state = {
       showModal: false
-    }
+    };
   }
 
   onClose = () => {
-    console.log('onClose is hit')
-    console.log('button state is', this.state)
+    console.log("onClose is hit");
+    console.log("button state is", this.state);
     this.setState({ showModal: false });
-  }
+  };
 
   onOpen = () => {
     this.setState({ showModal: true });
-  }
-
-
+  };
 
   render() {
     const image = this.props.image;
@@ -32,20 +29,31 @@ class Header extends Component {
             <Modal show={this.state.showModal} onHide={this.onClose}>
               <Modal.Header>
                 <div className="modal-close">
-                <div className="closeButton" onClick={this.onClose}>
-                  <div className="title">CLOSE</div>
-                </div>
+                  <div className="closeButton" onClick={this.onClose}>
+                    <div className="title">CLOSE</div>
+                  </div>
 
-                {/* <i className="fa fa-window-minimize" aria-hidden="true" onCLick={this.onClose}></i> */}
-              </div>
+                  {/* <i className="fa fa-window-minimize" aria-hidden="true" onCLick={this.onClose}></i> */}
+                </div>
                 <Modal.Title>About</Modal.Title>
               </Modal.Header>
               <Modal.Body>
-                <h4>Being born in the 90s, Hip-Hop has always been a huge part of my identity growing up. As a rap enthusiast, I’ve always been fascinated by how artists use rap as a vessel for public sentiment, from praising the lavish to shedding light on the impoverish. Naturally with all rap encompasses, especially politically, rap has become a convenient scapegoat throughout the years for violence and misfortune. To understand rap, it is important to perceive rap as as reflection of reality and a state of mind. Rapsmatter is dedicated to spreading the positive vibes in rap. Spread the inspiration.</h4>
+                <h4>
+                  Being born in the 90s, Hip-Hop has always been a huge part of
+                  my identity growing up. As a rap enthusiast, I’ve always been
+                  fascinated by how artists use rap as a vessel for public
+                  sentiment, from praising the lavish to shedding light on the
+                  impoverish. Naturally with all rap encompasses, especially
+                  politically, rap has become a convenient scapegoat throughout
+                  the years for violence and misfortune. To understand rap, it
+                  is important to perceive rap as as reflection of reality and a
+                  state of mind. Rapsmatter is dedicated to spreading the
+                  positive vibes in rap. Spread the inspiration.
+                </h4>
               </Modal.Body>
               <Modal.Footer>
                 <div className="modal-credit">
-                <h4>Created by @RxNeu</h4>
+                  <h4>Created by @BoyReads Instagram @Boytoy</h4>
                 </div>
                 <div className="nav-contain">
                   {/* <a href="https://www.facebook.com/rapsmatter/" target="_blank"><div className="fa fa-facebook btn btn-round"></div></a>
@@ -63,7 +71,7 @@ class Header extends Component {
           {/* <div className="sidebarCenter" onClick={this.onOpen}>
             <i className="fa fa-plug twitter" aria-hidden="true"></i>
           </div> */}
-            {/* <div className="sidebarBottom">
+          {/* <div className="sidebarBottom">
               <a href="https://twitter.com/RxNeu" target="_blank">
               <i className="fa fa-twitter twitter" aria-hidden="true"></i>
               </a>
@@ -85,7 +93,9 @@ class Header extends Component {
           {/* <div className="fa fa-facebook btn btn-round"><a href="/"></a></div>
           <div className="fa fa-twitter btn btn-round"><a href="https://twitter.com/RxNeu" target="_blank"></a></div>
           <div className="fa fa-instagram btn btn-round"><a href=""  target="_blank"></a></div> */}
-          <div className="fa fa-eye btn btn-round" onClick={this.onOpen}><a href="#" ></a></div>
+          <div className="fa fa-eye btn btn-round" onClick={this.onOpen}>
+            <a href="#" />
+          </div>
 
           {/* <div className="img-polaroid">
             <div onClick={this.onOpen}>
@@ -97,7 +107,6 @@ class Header extends Component {
             </div>
           </div> */}
         </div>
-
       </div>
     );
   }
--- src/components/RapLyric.js
+++ src/components/RapLyric.js
@@ -1,35 +1,41 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class RapLyric extends Component {
-	render() {
-    const lyric = this.props.lyric
-		const artist = this.props.artist
-		const songName = this.props.songName
-		console.log('artist in RapLyric', artist)
-		// console.log('lyric is ', lyric)
-    const lyricContent = lyric.split(' ');
+  render() {
+    const lyric = this.props.lyric;
+    const artist = this.props.artist;
+    const songName = this.props.songName;
+    console.log("artist in RapLyric", artist);
+    // console.log('lyric is ', lyric)
+    const lyricContent = lyric.split(" ");
     const lyricString = "";
-    const tweet = 'https://twitter.com/intent/tweet?text='+ '"' + lyric + '"' +'-'+ artist +'%20%40Rapsmatter&url=http%3A%2F%2Fwww.rapsmatter.com%2F&hashtags=rapsmatter&related=rxneu'
+    const tweet =
+      "https://twitter.com/intent/tweet?text=" +
+      '"' +
+      lyric +
+      '"' +
+      "-" +
+      artist +
+      "%20%40Rapsmatter&url=http%3A%2F%2Frapsmatter.herokuapp.com%2F&hashtags=rapsmatter&related=boyreads";
     // console.log('tweet is', tweet)
 
-
-		return (
-		  <div className="col-md-6 rapLyric">
-				<div className="quoteContainer">
-					<div className="quote">
-						<div className="gutter-lyric">{songName}</div>
-							<h2>{lyric}</h2>
-						<div className="rapTweet hidden-xs">
-							<a href={tweet} target="_blank">
-							<i className="fa fa-twitter twitter" aria-hidden="true"></i>
-							</a>
-						</div>
-						</div>
-				</div>
-		  </div>
+    return (
+      <div className="col-md-6 rapLyric">
+        <div className="quoteContainer">
+          <div className="quote">
+            <div className="gutter-lyric">{songName}</div>
+            <h2>{lyric}</h2>
+            <div className="rapTweet hidden-xs">
+              <a href={tweet} target="_blank">
+                <i className="fa fa-twitter twitter" aria-hidden="true" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     );
-	}
+  }
 }
 // a href="https://twitter.com/intent/tweet?text=hello"
 
